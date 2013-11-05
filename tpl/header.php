074a1a007bd41302130b0674833d362212395240
Mattia Marco <kuttebo@msn.com>
2013-11-05 16:06:01 +0100

    Update header.php
    Debug, pointing at correct css files now. (relative links)

--- tpl/header.php
+++ tpl/header.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">        
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
         <link href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css" />
-        <link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/css/default.css" rel="stylesheet" />
-        <link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/themes/<?php echo $theme; ?>.css" rel="stylesheet" />
+        <link href="css/default.css" rel="stylesheet" />
+        <link href="themes/<?php echo $theme; ?>.css" rel="stylesheet" />
     </head>
     <body>
         <header><h1><a href="index.php"><?php echo $blog_title; ?></a></h1></header>
